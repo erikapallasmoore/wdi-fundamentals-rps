64fb641bbb33405bb31593e224f2ad5e82e72929
Erika Moore <ErikaPallasMoore@Erikas-MacBook-Pro.local>
2015-11-02 14:10:10 -0500

    missingUpdate

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -72,7 +72,7 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    // Write code  that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
 
     console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove)
